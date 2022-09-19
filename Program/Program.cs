4f7b4a32bc01962c09e23d6a9737b522bfee7e20
Tatiana <tatiana-bondareva@inbox.ru>
2022-09-20 00:06:08 +0300

    change Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите строки через пробел: ");
+string? text = Console.ReadLine();
+string[] array = text!.Split(' ');
+
+// Поиск количества элементов, длина которых менее 4
+int FindElement(string[]array)
+{
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++)
+    if (array[i].Length < 4) count++;
+    return count;
+}
+
+// Формирование нового массива
+string[] CopyArray(string[] array, int size)
+{
+    int j = 0;
+    string[] newArray = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) 
+        {
+            newArray[j] = array[i];
+            j++;
+        }     
+    }
+    return newArray;
+}
+
+int size = FindElement(array);
+string[] newArray = CopyArray(array, size);
+
+Console.WriteLine("Исходный массив: [" + string.Join(", ", array) + ']'); 
+Console.WriteLine("Новый массив: [" + string.Join(", ", newArray) + ']');
